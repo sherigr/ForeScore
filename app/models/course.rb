91ae4744ca360b9e7cafdb95a1df004d9256efd1
sherigr <SGrossman00@gmail.com>
2015-01-20 18:35:17 -0500

    relationships added, db columns added

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,12 @@
+'require pry'
+'require HTTParty'
+
 class Course < ActiveRecord::Base
+
+	has_many :scorecards
+
+BASE_URL = http://theapi.active.com/golf/84/ListTeetimes?courseId=CourseId&date=Date&api_key=mmyhymr5cuhx9ceeg5pdy7h6
+
+
+
 end
--- app/models/scorecard.rb
+++ app/models/scorecard.rb
@@ -1,2 +1,6 @@
 class Scorecard < ActiveRecord::Base
+
+	belongs_to :user
+	belongs_to :course
+
 end
