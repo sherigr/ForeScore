83b61d50b872e5a14c83b0d22653acdcb532a461
sherigr <SGrossman00@gmail.com>
2015-01-26 22:41:16 -0500

    Scorecard issues since commit that it was working

--- app/models/scorecard.rb
+++ app/models/scorecard.rb
@@ -18,3 +18,14 @@ class Scorecard < ActiveRecord::Base
 
 end
 
+# def total_score
+# 		total_score = (":hole1" + ":hole2" + ":hole3" + ":hole4" + ":hole5" + ":hole6" + 
+# 		":hole7" + ":hole8" + ":hole9" + ":hole10" + ":hole11" + ":hole12" + ":hole13" +
+# 		":hole14" + ":hole15" + ":hole16" + ":hole17" + ":hole18")
+# 	end
+
+# 	def total_par
+# 		total_par = (":par1" + ":par2" + ":par3" + ":par4" + ":par5" + ":par6" + 
+# 		":par7" + ":par8" + ":par9" + ":par10" + ":par11" + ":par12" + ":par13" +
+# 		":par14" + ":par15" + ":par16" + ":par17" + ":par18")
+# 	end
