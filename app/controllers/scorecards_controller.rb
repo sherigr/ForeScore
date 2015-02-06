5f9bc1e4464abaf104bb803fb47e99bfd9b99bcb
sherigr <SGrossman00@gmail.com>
2015-02-06 11:09:02 -0500

    Fonts and other styling

--- app/controllers/scorecards_controller.rb
+++ app/controllers/scorecards_controller.rb
@@ -76,17 +76,18 @@ class ScorecardsController < ApplicationController
     end
   end
 
-def destroy
-  @scorecard = Scorecard.find(params[:id]) 
-  if @scorecard.user != current_user
-    redirect_to @scorecard
-  else
-    @scorecard.destroy
-    redirect_to scorecard_path
-    # render json: @scorecards 
+  def destroy
+    @scorecard = Scorecard.find(params[:id]) 
+    if @scorecard.user != current_user
+      redirect_to @scorecard
+    else
+      @scorecard.destroy
+      redirect_to scorecard_path
+      # render json: @scorecards 
+    end
   end
-end
 
+ 
 
   private 
 
